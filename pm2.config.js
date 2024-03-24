4a0819f8e23e3b3ce530a1b8e28f07796e9c8a6c
ariftoteles <arif.santoso4@bithealth.com>
2024-03-24 16:28:36 +0700

    create pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nextjs-app',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 6002',
+      instances: 'max',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
